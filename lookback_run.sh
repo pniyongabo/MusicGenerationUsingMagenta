7bea09a7b5c02fbde75ecd850cb16d7965aeff32
jmcarlson0320 <57571031+jmcarlson0320@users.noreply.github.com>
2021-06-10 16:20:44 -0700

    Update lookback_run.sh

--- lookback_run.sh
+++ lookback_run.sh
@@ -4,7 +4,7 @@ CONFIG=lookback_rnn
 melody_rnn_generate \
 --config=${CONFIG} \
 --bundle_file=${BUNDLE_PATH} \
---output_dir=/home/jmc/pdx/machine_learning/melody/attention/take_on_me/ \
+--output_dir=/home/jmc/pdx/machine_learning/melody/lookback/take_on_me/ \
 --num_outputs=10 \
 --num_steps=128 \
 --primer_melody="[60, 60, 60, 60, 67, 67, 67, 67, 69, 69, 69, 69, 67, 67, 69, 67]"
