82941c74392bcff7666b8a9411889e1d9b9a2b26
jmcarlson0320 <57571031+jmcarlson0320@users.noreply.github.com>
2021-06-10 16:16:05 -0700

    Add files via upload

---
+++ lookback_run.sh
@@ -0,0 +1,10 @@
+BUNDLE_PATH=/home/jmc/pdx/machine_learning/melody/lookback_rnn.mag
+CONFIG=lookback_rnn
+
+melody_rnn_generate \
+--config=${CONFIG} \
+--bundle_file=${BUNDLE_PATH} \
+--output_dir=/home/jmc/pdx/machine_learning/melody/attention/take_on_me/ \
+--num_outputs=10 \
+--num_steps=128 \
+--primer_melody="[60, 60, 60, 60, 67, 67, 67, 67, 69, 69, 69, 69, 67, 67, 69, 67]"
